d47061b92f0d484d029a75d93f975f86b495f91d
kosuke-furuta <furu.21.22@gmail.com>
2020-12-29 06:12:56 +0900

    finished task-pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,49 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "「#{@task.product_name}」を登録しました。"
+      redirect_to @task
+    else
+      render 'task/new'
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    flash[:success] = "「#{@item.product_name}」を編集しました。"
+    redirect_to tasks_url
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    flash[:success] = "「#{@task}」を削除しました。"
+    redirect_to tasks_url
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:product_name, :order_number,
+                                 :delivery_date, :quantity,
+                                 :process, :remarks)
   end
 end
