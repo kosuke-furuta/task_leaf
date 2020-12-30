28cd4b7eeacc3ead6472952b9364e26a67a20d61
kosuke-furuta <furu.21.22@gmail.com>
2020-12-30 09:13:08 +0900

    finished static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  
+  resources :tasks
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
